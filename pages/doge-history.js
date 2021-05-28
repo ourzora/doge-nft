2df4c928c989c0a40b1fde7f6743a7987ec6a431
Dain Blodorn Kim <cyberzone@dains-imac-2.lan>
2021-05-28 14:53:56 -0700

    updates

--- pages/doge-history.js
+++ pages/doge-history.js
@@ -1,15 +1,20 @@
 import Head from '../components/head'
 import GoHome from '../components/go-home'
-import AboutCopy from '../components/about-copy'
+import CopyBlock from '../components/doge-copy'
 import Image from 'next/image'
 import DogeFooter from '../components/doge-footer'
+
+import copy from '../lib/about-copy'
 
 const DogeHistory = () => {
   return (
     <>
-      <Head title="ABOUT DOGE" />
+      <Head title="Aout Doge"/>
       <GoHome />
-      <AboutCopy />
+      <CopyBlock
+        title="The History of Doge: From Humble Shiba to Meme Legend"
+        copy={copy}
+      />
       <aside className="doge-history_bg"></aside>
       <a href="https://knowyourmeme.com/" target="_blank" className="doge-history_kym">
         <p>Written and prepared by Know Your Meme</p>
