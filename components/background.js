0c22932968519f2201a9e0ed22f6d288e1bb410f
Dain Blodorn Kim <dain@db13.us>
2021-06-05 21:54:30 -0700

    not inlining brush svg

--- components/background.js
+++ components/background.js
@@ -126,19 +126,17 @@ const Background = () => {
 
   return (
     <aside className="doge-bg">
-      {/*
-        <div className="strokes">
-          <div className="paint-1-wrapper">
-            <Stroke className="paint-1 light-shadow" />
-          </div>
-          <div className="paint-2-wrapper">
-            <Stroke className="paint-2 light-shadow" />
-          </div>
-          <div className="paint-3-wrapper">
-            <Stroke className="paint-3 light-shadow" />
-          </div>
+      <div className="strokes">
+        <div className="paint-1-wrapper">
+          <img src="/svg/brush-stroke-1.svg" className="light-shadow"/>
         </div>
-      */}
+        <div className="paint-2-wrapper">
+          <img src="/svg/brush-stroke-2.svg" className="light-shadow"/>
+        </div>
+        <div className="paint-3-wrapper">
+          <img src="/svg/brush-stroke-1.svg" className="light-shadow"/>
+        </div>
+      </div>
       <div id="words">
         {words.map((word, i) => (
           <div
