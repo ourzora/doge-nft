12e966c69c9a3dd76869d0fa5dbf9bbc556f3e22
Dain M. Blodorn Kim <dain@db13.us>
2023-09-25 13:17:52 -0700

    revert to updated nft components

--- pages/doge/[id].js
+++ pages/doge/[id].js
@@ -1,8 +1,7 @@
 import Head from "../../components/head";
 import GoHome from "../../components/go-home";
 import find from "lodash/find";
-import { NFTFullPage, MediaConfiguration, FullComponents, NFTDataProvider } from "@derpyvision/doge-nft-components";
-import { MediaFetchAgent, Networks } from "@zoralabs/nft-hooks";
+import { NFTFullPage, MediaConfiguration } from "@zoralabs/nft-components";
 import DogeFooter from "../../components/doge-footer";
 import { NFT_LIST } from "../../lib/constants";
 
@@ -34,30 +33,13 @@ const Doge = ({ post }) => {
     showPerpetual: false,
     refreshInterval: false,
   };
-
-  // console.log(nftData?.description)
-
-  /*
-    description
-  : 
-  "One of the many classic photographs of the Shiba Inu “Kabosu,” this image was taken by her owner Atsuko Sato on February 13th, 2010, before going on to become an iconic meme throughout the 2010s. Snapped back when Kabosu was just a few years old during an impromptu photoshoot, this picture captures a rare side of the beloved Shiba behind the Doge meme."
-  mimeType
-  : 
-  "image/png"
-  name
-  : 
-  "Curious Doge"
-  */
-
   return (
     <>
-      {/*
       <Head
         title={post.title}
-        description={nftData.description}
-        ogImage={`${nftData.zoraNFT.contentURI}`}
+        description={post?.description}
+        ogImage={post?.image}
       />
-       */}
       <GoHome />
       <article
         className={`doge-nft_wrapper`}
@@ -87,26 +69,9 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const post = find(NFT_LIST, { slug: params.id });
-  const fetcher = new MediaFetchAgent(Networks.MAINNET);
-
-  // const nft = await fetcher.loadZNFTData(post.id);
-  // const metadata = await fetcher.fetchIPFSMetadata(nft.nft.metadataURI);
-  /*
-  console.log(nft)
-
-  function prepareJson(json) {
-    return JSON.parse(JSON.stringify(json));
-  }
-
-  const nftData = prepareJson({
-    ...nft,
-    ...metadata,
-  });
-  */
   return {
     props: {
       post,
-      // nftData,
     },
   };
 }
