4b3e6533792b0e3c042ee837e1335822b5325501
Dain M. Blodorn Kim <dain@db13.us>
2023-09-25 13:29:05 -0700

    update nextjs and react and add lodash import

--- components/go-home.js
+++ components/go-home.js
@@ -5,11 +5,9 @@ import Stroke from '../public/svg/brush-stroke.svg'
 const GoHome = () => {
   return (
     <div className="go-home_wrapper">
-      <Link href="/">
-        <a className="go-home_link">
-          <DogeHead/>
-          <span className="go-home_cta lozenge-button">Go Home</span>
-        </a>
+      <Link href="/" className="go-home_link">
+        <DogeHead/>
+        <span className="go-home_cta lozenge-button">Go Home</span>
       </Link>
       <div className="go-home_paint-wrapper">
         <Stroke className="go-home_paint" />
